fcd8837919930d4585cd48a35e6bc2c23c37702f
qianfeng <qianfeng@lmd.local>
2015-12-15 09:05:46 +0800

    Initial Commit

---
+++ NBABattle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NBABattle
+//
+//  Created by qianfeng on 15/12/15.
+//  Copyright © 2015年 高歌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
